e410b7d18fbe17d6aa9417cda363774789a79ba2
oszkar0 <oskszysiak@gmail.com>
2024-04-01 23:26:10 +0200

    46. Permutations

---
+++ permutations.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+
+        if len(nums) == 1:
+            return [nums[:]]
+        
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+
+            for perm in perms:
+                perm.append(n)
+
+            result.extend(perms)
+            nums.append(n)
+        
+        return result
