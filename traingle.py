bfd2920e925730294b0c6e4cb69c42c1421d8efb
oszkar0 <oskszysiak@gmail.com>
2024-04-12 12:10:19 +0200

    120. Triangle

---
+++ traingle.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        dp = [0] * (len(triangle) + 1)
+
+        for row in triangle[::-1]:
+            for i, n in enumerate(row):
+                dp[i] = n + min(dp[i], dp[i + 1])
+
+        return dp[0]
