3d60632175dbe0360994ac3e1073b35234e60ac2
oszkar0 <oskszysiak@gmail.com>
2024-04-12 12:40:58 +0200

    64. Minimum Path Sum

---
+++ minimum_path_sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        ROWS, COLS = len(grid), len(grid[0])
+        
+        res = [[float('inf')] * (COLS + 1) for r in range (ROWS + 1)]
+        res[ROWS - 1][COLS] = 0
+
+        for r in range(ROWS - 1, -1, -1):
+            for c in range(COLS - 1, -1, -1):
+                res[r][c] = min(res[r + 1][c], res[r][c + 1]) + grid[r][c]
+        
+        return res[0][0]
